4de13dc5889d898a3c90f941807d0204aa63b04a
pasDamola <oyindamola.oyetunmibi@outsourceglobal.com>
2020-04-24 11:38:07 +0100

    result function in prog...

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -28,7 +29,7 @@ def player(board):
     #if it is not initial state and X has played then return O
     elif board != initial_state() and turn == X:
         turn = O
-    return player
+    return turn
 
 
 
@@ -41,7 +42,7 @@ def actions(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             #check if any of the cells are empty and add them to the set
-            if board[i][j] != 'X' and board[i][j] != 'O':
+            if board[i][j] != X and board[i][j] != O:
                 possible_actions.add((i,j))
     return possible_actions
                 
@@ -53,7 +54,17 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    new_board = copy.deepcopy(board)
+    player = player(new_board)
+    possible_moves = actions(new_board)
+    possible_moves = list(possible_moves)
+    action = possible_moves[0]
+    i = action[0]
+    j = action[1]
+    new_board[i][j] = player
+    return new_board
+
+    
 
 
 def winner(board):
